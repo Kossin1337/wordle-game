973b166d9aa7c4f8194729aa5d6845ce489a1c00
Kossin1337 <piotrswobodzinski@gmail.com>
2022-05-28 08:09:06 +0200

    json-server & fetching random word

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        /* random int between 0 -> jsonData.length - 1 */
+        const randomSolution =
+          jsonData[Math.floor(Math.random() * jsonData.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div>
       <h1>Wordle</h1>
+      {solution && <span>Solution | {solution}</span>}
     </div>
   );
 };
